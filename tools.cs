e61f97315291a2316d823afe9aa73d30340a04a6
李全乐（金迪 ） <liquanle@egova.com.cn>
2023-02-19 15:35:39 +0800

    toolname used getToolName method

---
+++ tools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataClient
+{
+    internal class tools
+    {
+    }
+
+    public class eatTool
+    {
+        public string toolname;
+        public string usedescription;
+
+        public void getToolName()
+        {
+
+        }
+    }
+}
